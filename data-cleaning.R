a6ca9483694c52bcd171af6fa423e20824f32b21
Jack Regan <jackregan10@icloud.com>
2024-11-25 13:19:25 -0500

    Corrected splitting function for imported datasets

--- data-cleaning.R
+++ data-cleaning.R
@@ -12,18 +12,18 @@ voting_population <- read.csv("data/population/ACSDP5Y2022.DP05-Data.csv")
 ##Clean Data
 household_income <- household_income|>
   select(NAME, S2001_C01_014E) |>
-  slice(-1:-2) |>
+  slice(0:-1) |>
   mutate(NAME  = gsub(", North Carolina", "", NAME)) |>
   rename(County = NAME, Mean_Earnings = S2001_C01_014E)
 
 voting_population <- voting_population |>
   select(NAME, DP05_0021E) |>
-  slice(-1:-2) |>
+  slice(0:-1) |>
   mutate(NAME  = gsub(", North Carolina", "", NAME)) |>
   rename(County = NAME, Eligible_Population = DP05_0021E)
 
 education <- education |>
-  slice(-1:-2) |>
+  slice(0:-1) |>
   mutate(
     NAME  = gsub(", North Carolina", "", NAME),
     HS_Graduation_Rate = (as.numeric(S1501_C01_003E) + as.numeric(S1501_C01_004E) + 
